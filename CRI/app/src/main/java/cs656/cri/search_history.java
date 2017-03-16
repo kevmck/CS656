27fb886e53098b57d9aa5af59e1280c5f309055a
Theresa Wagner <tlw7@njit.edu>
2017-03-15 22:05:54 -0400

    added buttons on main interface, added years to search, added menu options on top right

---
+++ CRI/app/src/main/java/cs656/cri/search_history.java
@@ -0,0 +1,13 @@
+package cs656.cri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class search_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_history);
+    }
+}
