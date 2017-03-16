6f237de0f8f563db87034c21f93faafed90f6445
Theresa Wagner <tlw7@njit.edu>
2017-03-15 22:34:12 -0400

    Made the history menu, just formatted and stuff

--- CRI/app/src/main/java/cs656/cri/search_history.java
+++ CRI/app/src/main/java/cs656/cri/search_history.java
@@ -9,5 +9,11 @@ public class search_history extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
