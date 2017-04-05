0fe9ec68e05fdaefba503158019d1189c89fd3f8
kevmck <klm25@njit.edu>
2017-04-05 11:52:12 -0400

    Year spinner works.

--- CRI/app/src/main/java/cs656/cri/ApiRequest.java
+++ CRI/app/src/main/java/cs656/cri/ApiRequest.java
@@ -3,71 +3,102 @@ package cs656.cri;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
 import javax.json.*;
 import java.io.StringReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
-class ApiRequest implements Runnable
-{
+class ApiRequest implements Runnable {
 
-    public static void api()
+    Handler h;
+    ArrayList<String> dataArrayList = new ArrayList<String>();
+
+    public ApiRequest (Handler h){
+        this.h = h;
+    }
+
+
+    public void api()
     {
-        //Test inputs to pull data from the cache/API.
         URL apiLink = null;
-        try{
-            apiLink= new URL("https://one.nhtsa.gov/webapi/api/Recalls/vehicle?format=json");
-        } catch (MalformedURLException e){
+        try
+        {
+            apiLink = new URL("https://one.nhtsa.gov/webapi/api/Recalls/vehicle?format=json");
+        }
+
+        catch (MalformedURLException e)
+        {
             System.out.println('0');
         }
+
         URLConnection apiRequest = null;
-        try {
+        try
+        {
             apiRequest = apiLink.openConnection();
-        } catch (IOException e1){
+        }
+        catch (IOException e1)
+        {
             System.out.println("1");
         }
+
         BufferedReader in = null;
-        try {
+        try
+        {
             in = new BufferedReader(new InputStreamReader(apiRequest.getInputStream()));
-        } catch (IOException e2){
+        }
+        catch (IOException e2)
+        {
             System.out.println("2");
         }
-        String inputLine;
-        ArrayList<String> spinnerYear = new ArrayList<String>();
 
-        try {
-            while ((inputLine = in.readLine()) != null) {
+        String inputLine;
+
+        try
+        {
+            while ((inputLine = in.readLine()) != null)
+            {
                 JsonReader jsonReader = Json.createReader(new StringReader(inputLine));
                 JsonObject jsonObj = jsonReader.readObject();
                 JsonArray yearList = jsonObj.getJsonArray("Results");
 
-                for (JsonObject year : yearList.getValuesAs(JsonObject.class)) {
-                    spinnerYear.add(year.getString("ModelYear"));
+                for (JsonObject year : yearList.getValuesAs(JsonObject.class))
+                {
+                    if (!year.getString("ModelYear").equals("9999"))
+                        dataArrayList.add(year.getString("ModelYear"));
                 }
 
             }
-        } catch(IOException e){
-            System.out.println("3");
         }
-
-        try {
-        in.close();
-        } catch (IOException e2){
-            System.out.println("4");
-        }
-
-        for (int i=1; i < spinnerYear.size(); i++)
+        catch(IOException e)
         {
-            System.out.println(spinnerYear.get(i));
+            System.out.println("IOException");
+        }
+
+        try
+        {
+            in.close();
+        }
+        catch (IOException e2)
+        {
+            System.out.println("4");
         }
 
     }
 
-    @Override
-    public void run() {
+
+    public void run()
+    {
         this.api();
+        Message msg = new Message();
+        Bundle data = new Bundle();
+        data.putStringArrayList("result", dataArrayList);
+        msg.setData(data);
+        if(h != null)
+            h.sendMessage(msg);
     }
 }
--- CRI/app/src/main/java/cs656/cri/carSearch.java
+++ CRI/app/src/main/java/cs656/cri/carSearch.java
@@ -1,7 +1,11 @@
 package cs656.cri;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Message;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,15 +13,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.logging.LogRecord;
 
 public class carSearch extends AppCompatActivity {
 
     private Button backButton;
+    private Spinner mYearSpinner;
+    private Spinner mMakeSpinner;
+    private Spinner mModelSpinner;
+
+    private Handler mYearHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -34,10 +49,7 @@ public class carSearch extends AppCompatActivity {
         });
 
 */
-
-
-        //ApiRequest.api();
-        new Thread(new ApiRequest()).start();
+        defineHandlers();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,11 +60,43 @@ public class carSearch extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        this.mYearSpinner = (Spinner) this.findViewById(R.id.spinner);
+        //ApiRequest.api();
+        new Thread(new ApiRequest(mYearHandler)).start();
+
     }
+
+
 
     private void launchActivity()
     {
         Intent intent = new Intent(this, SearchResults.class);
         startActivity(intent);
     }
+
+
+
+
+    private void changeSpinner(ArrayList<String> arrayList){
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, arrayList);
+        this.mYearSpinner.setAdapter(yearAdapter);
+    }
+
+
+    private void defineHandlers(){
+        mYearHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg){
+                //TODO
+                super.handleMessage(msg);
+                Bundle data = msg.getData();
+                ArrayList<String> aList = data.getStringArrayList("result");
+                changeSpinner(aList);
+                System.out.println("Message is received!");
+            }
+        };
+    }
+
 }
